aae0c5b2785bd3ca65c7c1b2b48650ef2479c74f
Yulchick <yulchick88@gmail.com>
2022-10-21 03:59:23 +0300

    Updated
    Modified and created files.

--- src/App.js
+++ src/App.js
@@ -1,129 +1,24 @@
-import { useState } from "react";
-import img2min from "./img2min.png";
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./Pages/About";
+import Homepage from "./Pages/Homepage";
 import "./App.css";
 
 function App() {
-  const [holiday, setHoliday] = useState("");
-  const [inputDay, setInputDay] = useState("");
-  const [inputMonth, setInputMonth] = useState("");
-  const [inputYear, setInputYear] = useState("");
-  const [inputCountry, setInputCountry] = useState("");
-  const [location, setLocation] = useState("");
-  const [newDate, setNewDate] = useState("");
-  const [weekDay, setWeekDay] = useState("");
-  const [error, setError] = useState(false);
-  const [displayStyle, setDisplayStyle] = useState({ display: "" });
-
-  const upDateHoliday = () => {
-    fetch(
-      "https://holidays.abstractapi.com/v1/?api_key=29c0b7f59f0f4553a3224afc01dd02f2&country=" +
-        inputCountry +
-        "&year=" +
-        inputYear +
-        "&month=" +
-        inputMonth +
-        "&day=" +
-        inputDay +
-        ""
-    )
-      .then((el) => el.json())
-      .then((data) => {
-        setHoliday(data[0].name);
-        setInputCountry(data[0].country);
-        setLocation(data[0].location);
-        setNewDate(inputDay + "-" + inputMonth + "-" + inputYear);
-        setWeekDay(data[0].week_day);
-        setDisplayStyle({ display: "flex" });
-        setError(false);
-      })
-      .catch((error) => {
-        setError(true);
-        setDisplayStyle({ display: "none" });
-      });
-  };
-
-  const style = { display: displayStyle.display };
-
   return (
-    <div className="wrapper">
-      <header className="title">National holidays</header>
-      <div className="input-info-block">
-        <div className="input-info">
-          <label className="input-info-label">
-            Enter country code
-            <input
-              type="text"
-              className="input"
-              value={inputCountry}
-              onChange={(event) => {
-                setInputCountry(event.target.value);
-              }}
-              placeholder="UA"
-            />
-          </label>
-        </div>
-        <div className="input-info">
-          <label className="input-info-label">
-            Enter number day
-            <input
-              type="text"
-              className="input"
-              value={inputDay}
-              onChange={(event) => {
-                setInputDay(event.target.value);
-              }}
-            />
-          </label>
-        </div>
-        <div className="input-info">
-          <label className="input-info-label">
-            Enter number month
-            <input
-              type="text"
-              className="input"
-              value={inputMonth}
-              onChange={(event) => {
-                setInputMonth(event.target.value);
-              }}
-            />
-          </label>
-        </div>
-        <div className="input-info">
-          <label className="input-info-label">
-            Enter year
-            <input
-              type="text"
-              className="input"
-              value={inputYear}
-              onChange={(event) => {
-                setInputYear(event.target.value);
-              }}
-            />
-          </label>
-        </div>
-        <button type="button" className="btn-info" onClick={upDateHoliday}>
-          Search
-        </button>
-      </div>
-      {error && <div className="error">Not found</div>}
-      {holiday && (
-        <div className="block-info" style={style}>
-          <div className="text-info">
-            Country: <span className="text-answer">{location}</span>
-          </div>
-          <div className="text-info">
-            Holiday: <span className="text-answer">{holiday}</span>
-          </div>
-          <div className="text-info">
-            Date: <span className="text-answer">{newDate}</span>
-          </div>
-          <div className="text-info">
-            Week Day: <span className="text-answer">{weekDay}</span>
-          </div>
-        </div>
-      )}
-      <img src={img2min} alt="holidays" className="image-bg" />
-    </div>
+    <>
+      <header className="header">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/about" className="nav-link">
+          About
+        </Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Pages/About.js
@@ -0,0 +1,14 @@
+import "./Pages.css";
+
+function About() {
+  return (
+    <div className="wrapper">
+      <h1 className="title">About us</h1>
+      <p className="text-info">
+        We make the world a better place when we work for you.
+      </p>
+    </div>
+  );
+}
+
+export default About;
